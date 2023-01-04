295b6b98a1c23c6216a2caca06d78d1fe32deb4a
SVHugo <hugosvalencia7@gmail.com>
2023-01-04 18:19:24 -0500

    se agregaron descripciones a los servicios y se adapto los texto

---
+++ script/agrandar_box_servicio.js
@@ -0,0 +1,28 @@
+/* var clickElment = new Array();
+var servicioInformacionEsqueleto = document.querySelectorAll(".box > .cajas")
+var caja_1 = document.querySelector("#caja1");
+var caja_prueba_1 = document.querySelector("#caja_prueba");
+console.log(caja_1);
+var arrayClass = new Array();
+console.log(servicioInformacionEsqueleto)
+ */
+/* document.onclick = addCssBoxForClick; */
+
+/* function addCssBoxForClick(e){
+    
+    haHechoClick = e.target.className;
+    haHechoCliclAdd = e;
+    console.log("*****"+haHechoClick)
+    if(haHechoClick == "cajas__card"){
+        caja_1.classList.add("prueba");
+        caja_prueba.classList.add("prueba_card")
+    }else if(haHechoClick == "cajas prueba"){
+        caja_1.classList.remove("prueba");
+        caja_prueba.classList.remove("prueba_card")
+    }
+
+
+}
+
+ */
+
--- script/bocetosCarrousel.js
+++
@@ -1,3 +0,0 @@
-
-
-const esqueleto_label = document.querySelector("#esqueleto_label");
